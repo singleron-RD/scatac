c0c117be78e19ed9be35841fd38dbef0cd9c9200
zhouyiqi91 <386346852@qq.com>
2024-05-29 16:46:41 +0800

    fix lint

--- bin/make_fragment.py
+++ bin/make_fragment.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+
 import snapatac2
 
 bam, sample = sys.argv[1:3]
 output_file = f"{sample}.fragment.gz"
-snapatac2.pp.make_fragment_file(
-    bam,
-    output_file,
-    barcode_regex="(\D*):\d*$"
-)
+snapatac2.pp.make_fragment_file(bam, output_file, barcode_regex="(\D*):\d*$")
